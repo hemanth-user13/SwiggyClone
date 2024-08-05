30c2675f7e4c077f477d68b60df0721f6dc8a47f
22210301016 <bolgamhemanth7878@gmail.com>
2024-08-05 22:09:21 +0530

    the navbar and footer are added

---
+++ src/Components/Main/Resturants.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Resturants() {
+  return (
+    <div>
+      <h2 className="text-left  text-2xl">
+        <b>Top restaurant chains in Hyderabad</b>
+      </h2>
+    </div>
+  );
+}
+
+export default Resturants;
